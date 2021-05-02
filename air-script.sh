77f6fd6e50459e8d6724a5cd15610f1ab204f77f
Liam Bendix <48177481+B3ND1X@users.noreply.github.com>
2021-05-02 12:15:28 -0400

    Update.

--- air-script.sh
+++ air-script.sh
@@ -68,7 +68,7 @@ echo -e "${Red}
          ░                                  "
 echo -e "${Yellow} \n             Hack the world!!!     "
 echo -e "${Green}\n                    Developed by: Liam Bendix"
-echo -e "${Green}                         Version: 1.0.6 Stable"
+echo -e "${Green}                         Version: 1.0.7 Stable"
 }
 
 menu () {        ##### Display available options #####
