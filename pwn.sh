1c8e3b650e605a4c1c8dcabb83805423c7db7a49
Liam Bendix <48177481+B3ND1X@users.noreply.github.com>
2021-06-02 00:57:51 -0400

    update

--- pwn.sh
+++ pwn.sh
@@ -433,7 +433,7 @@ aireplay-ng --deauth 0 -a $bssid $foo > /dev/null
 
 monitor () {        ##### Monitor mode, scan available networks & select target #####
 spinner &
-airmon-ng start $foo > /dev/null
+airmon-ng start $foo > /dev/null 2>&1 
 trap "airmon-ng stop $foo > /dev/null;rm generated-01.kismet.csv 2> /dev/null" EXIT 
 airodump-ng --output-format kismet --write generated $foo > /dev/null & sleep 20 ; kill $!
 sed -i '1d' generated-01.kismet.csv
